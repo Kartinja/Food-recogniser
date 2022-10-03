495cad30bbd4f21010b5d71d8919bf0f682baff9
Kartinja <deko.popovski@outlook.com>
2022-10-03 14:37:08 +0200

    First commit

---
+++ src/test/java/com/myapp/Food/recogniser/FoodRecogniserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.Food.recogniser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodRecogniserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
